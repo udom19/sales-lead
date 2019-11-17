2d6b01ccba8ff2fec199c54b15b6e6f0158bbab6
udom19 <udomcosmas@gmail.com>
2019-11-17 22:43:03 +0100

    add lead show edit and post routes

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,12 +1,50 @@
 const models = require('../models');
 exports.get_landing = function(req, res, next) {
-    res.render('landing', { title: 'Express' });
-  }
+    res.render('landing', { title: 'Lead\'s page' });
+}
 
-  exports.submit_lead = function(req, res, next) {
-    return models.Lead.create({
-      email: req.body.lead_email
-    }).then(lead => {
-      res.redirect('/');
-    })
-  }
+exports.submit_lead = function(req, res, next) {
+  return models.Lead.create({
+    email: req.body.lead_email
+  }).then(lead => {
+    res.redirect('/leads');
+  })
+}
+
+exports.show_leads = function(req, res, next) {
+  return models.Lead.findAll().then(leads => {
+    res.render('landing', { title: 'Leads page', leads: leads });
+  })
+}
+
+exports.show_lead = function(req, res, next) {
+  return models.Lead.findOne({
+    where: {
+      id: req.params.lead_id
+    }
+  }).then(lead => {
+    res.render('lead', { lead: lead });
+  })
+}
+
+exports.show_edit_lead = function(req, res, next) {
+  return models.Lead.findOne({
+    where: {
+      id: req.params.lead_id
+    }
+  }).then(lead => {
+    res.render('lead/edit_lead', { lead: lead });
+  })
+}
+
+exports.edit_lead = function(req, res, next) {
+  return models.Lead.update({
+    email: req.body.lead_email
+  }, {
+    where: {
+      id: req.params.lead_id
+    }
+  }).then(result => {
+    res.redirect('/lead/' + req.params.lead_id)
+  })
+}
